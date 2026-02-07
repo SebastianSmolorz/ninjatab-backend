8a07b6b33b1c5c13738e9efff71e53914d844bd5
sebsmolorz <seb@zenstores.com>
2026-02-07 18:15:29 +0000

    dotenv

--- ninjatab/settings/dev.py
+++ ninjatab/settings/dev.py
@@ -1,12 +1,13 @@
 from .base import *  # noqa: F401, F403
+from .base import env
 
 DEBUG = True
 
-SECRET_KEY = "django-insecure-3db7pm5*^nx3_#2ax=r$&mviqevr)rzyco3+m9bz38=vt=v1if"
+SECRET_KEY = env.str("SECRET_KEY", default="django-insecure-3db7pm5*^nx3_#2ax=r$&mviqevr)rzyco3+m9bz38=vt=v1if")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOWED_ORIGINS = env.list(
+    "CORS_ALLOWED_ORIGINS",
+    default=["http://localhost:3000", "http://127.0.0.1:3000"],
+)
