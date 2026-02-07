ecf93d38b7cfabd398494b677cb75b54269a5e3a
sebsmolorz <seb@zenstores.com>
2026-02-07 21:16:00 +0000

    Add prod static config

--- ninjatab/settings/prod.py
+++ ninjatab/settings/prod.py
@@ -19,3 +19,6 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=False)
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+STATIC_ROOT = "/var/www/ninjatab/static"
+MEDIA_ROOT = "/var/www/ninjatab/media"
