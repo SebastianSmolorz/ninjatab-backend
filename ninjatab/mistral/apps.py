dabef090107e5c08c15464a35ba69c41c2c4da5e
sebsmolorz <seb@zenstores.com>
2026-02-16 22:52:09 +0000

    Remove Mistral app

--- ninjatab/mistral/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MistralConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ninjatab.mistral"
--- ninjatab/settings/base.py
+++ ninjatab/settings/base.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "ninjatab.tabs",
-    "ninjatab.mistral",
 ]
 
 MIDDLEWARE = [
