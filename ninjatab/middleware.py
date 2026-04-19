ae652592bf97b673eb100574888599f8e8c4f3dd
sebsmolorz <seb@zenstores.com>
2026-04-19 21:20:09 +0100

    Posthog use uuid not id in middleware

--- ninjatab/middleware.py
+++ ninjatab/middleware.py
@@ -3,7 +3,18 @@ from posthog.integrations.django import PosthogContextMiddleware
 
 class AnonymousAwarePosthogMiddleware(PosthogContextMiddleware):
     def _resolve_user_details(self, user):
-        user_id, email = super()._resolve_user_details(user)
+        _, email = super()._resolve_user_details(user)
+        user_id = None
+
+        is_authenticated = getattr(user, "is_authenticated", False)
+        if callable(is_authenticated):
+            is_authenticated = is_authenticated()
+
+        if is_authenticated:
+            uuid = getattr(user, "uuid", None)
+            user_id = str(uuid) if uuid is not None else None
+
         if user_id is None:
             user_id = "anonymous"
+
         return user_id, email
